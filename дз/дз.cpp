264c191f5bad2b7ec1652233ba20439bb1c8617b
valek <vpp27v@gmail.com>
2024-09-05 13:21:50 +0300

    тест

--- дз/дз.cpp
+++ дз/дз.cpp
@@ -5,26 +5,27 @@ int main() {
     setlocale(LC_ALL, "Russian");
     int num1, num2, suma, minus, del, umn;
     string znak;
-    cout << ("");
+    cout << ("Введите первое число: ");
     cin >> num1;
-    cout << ("");
+    cout << ("Введите второе число: ");
     cin >> num2;
-    cout << ("") << endl;
+    cout << ("Выберите операцию\n +, -, *, / ") << endl;
     cin >> znak;
-    if (znak == "") {
+    if (znak == "+") {
         suma = num1 + num2;
-        cout << "" << suma << endl;
+        cout << "Результат суммы:\n" << suma << endl;
     }
-    else if (znak == "") {
+    else if (znak == "-") {
         minus = num1 - num2;
-        cout << "" << minus << endl;
+        cout << "Результат вычитания:\n" << minus << endl;
     }
-    else if (znak == "") {
+    else if (znak == "*") {
         umn = num1 * num2;
-        cout << " " << umn << endl;
+        cout << "Результат умножения:\n" << umn << endl;
     }
-    else if (znak == "") {
+    else if (znak == "/") {
         del = num1 / num2;
-        cout << " " << del << endl;
+        cout << "Результат деления:\n" << del << endl;
     }
+
 }
